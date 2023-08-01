493b041985b2bc550d146f8c09c7d5d2e42ebc17
Altermn <134489956+altermn@users.noreply.github.com>
2023-08-01 17:47:59 +0400

    New rules modal

--- src/components/RulesModal.vue
+++ src/components/RulesModal.vue
@@ -3,17 +3,36 @@
     <div class="flex modal-content">
       <div class=" flex message-modal-content">
         <div class="instructions">
-          <h2 class="instructions-title">How it works</h2>
-          <div>
-            <p> On Segmint you can colorify pixels on public board to get NFT</p>
-            <p>Each time the board is nearly full the next season begins.  And users will claim pixels over the old canvas</p>
-            <p>If your NFT is completely painted over, you can burn it and get your tokens back. Every season the price goes up linearly</p>
-            <p>Your nft can be locked in case of inappropriate content</p>
+          <h2 class="primary-text">How it works</h2>
+          <ul>
+            <li>
+              <p class='secondary-text'>Pick an area to take a seat on the board</p>
+            </li>
+            <li>
+              <p class='secondary-text'>Each time the board fills up, a new season begins</p>
+            </li>
+            <li>
+              <p class='secondary-text'>Each season increases the price per pixel</p>
+            </li>
+            <li>
+              <p class='secondary-text'>You can burn your NFT if it has been painted over completely and get your tokens back</p>
+            </li>
+          </ul>
+          <div class='instructions-warning'>
+            <img :src="`${publicPath}icons/warning.svg`" alt="link">
+            <h4 class='instructions-warning-text'>
+              Your nft can be locked in case of inappropriate content
+            </h4>
           </div>
+          <div class='instructions-line'></div>
         </div>
         <button class="primary-button" v-on:click="this.close">
           OK
         </button>
+        <div class='season'>
+          <h2 class='primary-text'>Season {{this.season}}</h2>
+          <p class='secondary-text'>1 pixel per {{ this.price }} VENOM</p>
+        </div>
       </div>
     </div>
   </modal>
@@ -23,10 +42,19 @@
 export default {
   name: 'RulesModal',
   data: function () {
-    return {}
+    return {
+      publicPath: process.env.BASE_URL,
+    }
   },
   props: ['name'],
-  computed: {},
+  computed: {
+    season: function() {
+      return this.$store.state.Provider.epoch;
+    },
+    price: function() {
+      return (parseInt(this.$store.state.Provider.currentTilePrice) / 100 / 1_000_000_000).toFixed(3)
+    }
+  },
   methods: {
     close() {
       this.$modal.hide(this.name);
@@ -37,74 +65,114 @@ export default {
 </script>
 
 <style scoped>
-.message-modal {
-  background-color: rgb(59 0 135 / 50%);
-  z-index: 9;
-}
-.modal-content {
-  position: fixed;
-  top: 15%;
-  left: calc(50% - 170px);
-  flex-direction: column;
-  width: 340px;
-  background-color: #21004B;
-  border: 10px solid #7000FF;
-  color: #CCFF00;
-  box-sizing: border-box;
-}
-.message-modal-content {
-  flex-direction: column;
-  padding: 20px;
-}
-.instructions {
+.primary-button {
+  width: 133px;
+  font-family: 'Chakra Petch';
+  font-style: normal;
   font-weight: 700;
   font-size: 24px;
-  margin: 0 20px;
-  text-align: left;
+  line-height: 30px;
 }
-.primary-button {
-  padding: 5px 50px;
-  margin-bottom: 20px;
-}
-.instructions-title {
-  margin: 20px 0;
-}
+
 .modal-content {
   position: fixed;
-  top: 15%;
-  left: 0;
+  top: 5%;
+  left: calc(50% - 225px);
   flex-direction: column;
-  width: 100%;
+  width: 450px;
   background-color: #21004B;
-  border: 5px solid #7000FF;
-  color: #CCFF00;
+  border: 10px solid #7000FF;
+  color: #CCFF01;
   box-sizing: border-box;
+  padding: 40px;
+}
+
+
+.message-modal-content {
+  flex-direction: column;
+  text-align: left;
+}
+
+.instructions-title {
+  font-size: 32px;
+  line-height: 38px;
+  margin: 0 0 24px 0;
+  font-weight: 600;
+}
+
+.modal-content ul {
+  padding-inline-start: 16px;
+}
+
+.modal-content li p {
+  margin: 24px 0;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.instructions-warning {
+  display: flex;
+  margin: 24px 5px;
+}
+.instructions-warning img {
+  margin-right: 11px;
+}
+.instructions-warning-text {
+  font-family: 'Chakra Petch';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 16px;
+  color: #CCFF01;
+  margin: 0;
+}
+.instructions-line {
+  margin: 24px 0;
+  width: 100%;
+  height: 1px;
+  background-color: rgba(204, 255, 1, 0.5);
+}
+.season {
+ margin-top: 24px;
+ text-align: center;
+}
+
+.primary-text {
+  font-size: 32px;
+  line-height: 40px;
+  font-weight: 600;
+  margin: 0;
+}
+.secondary-text {
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: 500;
+  margin: 0;
 }
 
 @media only screen and (max-width: 500px) {
   .modal-content {
     top: 0;
     bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
     flex: 1;
     justify-content: center;
+    padding: 10px;
   }
-  .modal-content p {
-    font-size: 20px;
+  .primary-text {
+    font-size: 28px;
+    line-height: 34px;
+    font-weight: 600;
+    margin: 0;
+  }
+  .secondary-text {
+    font-size: 18px;
+    line-height: 24px;
+    font-weight: 500;
+    margin: 0;
   }
 }
 
-@media only screen and (min-width: 500px) {
-  .modal-content {
-    top: 15%;
-    left: calc(50% - 250px);
-    width: 500px;
-    border: 10px solid #7000FF;
-  }
-}
-
-@media only screen and (max-width: 500px) {
-  .instructions h5 {
-    font-size: 16px !important;
-  }
-}
 </style>
